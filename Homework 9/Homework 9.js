04b590a97b08f4cb93f8cded942bafe983adc8bb
Wayne2420 <112452726+Wayne2420@users.noreply.github.com>
2022-11-13 16:00:13 -0700

    Add files via upload

---
+++ Homework 9/Homework 9.js
@@ -0,0 +1,43 @@
+
+function setup () {
+    createCanvas (400,400);
+}
+
+function draw() {
+    background (220);
+    let A = color('antiquewhite');
+    fill(A);
+    strokeWeight(4);
+    stroke(51);
+    square(140,120,150)
+    circle(200, 150, 200);
+    let C = color('blue');
+    fill(C);
+    square(149, 180, 100, 20);
+    let B = color('crimson');
+    fill (B);
+    strokeWeight(4);
+    stroke(51);
+    square(50,265,300)
+    triangle(130,270,134,350,30,270)
+    triangle(251,270,245,350,370,270)
+    let D = color('white');
+    fill (D);
+    circle(160,130,60)
+    circle(240,130,60)
+    let E = color('green');
+    fill (E);
+    circle(160,130,27)
+    circle(240,130,27)
+    let F = color('black');
+    fill (F);
+    line (130,90,190,90)
+    line (270,90,210,90)
+    
+
+
+
+
+
+
+}
